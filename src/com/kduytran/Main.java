f2fa591d6ff5a11201904fcf3d77b334dc823d22
kduytran443 <trankhanhduy18@gmail.com>
2024-02-26 21:46:49 +0700

    #testing: add test 4

--- src/com/kduytran/Main.java
+++ src/com/kduytran/Main.java
@@ -7,10 +7,14 @@ public class Main {
         PhoneNumber common2 = PhoneNumber.of(123, 1234);
         System.out.println(common == common2);
 
-        // Test
+        // Test3
         PhoneNumber common3 = PhoneNumber.of(120, 1234);
         System.out.println(common3);
-        
+
+        // Test4
+        PhoneNumber common4 = PhoneNumber.of(120, 1234);
+        System.out.println(common4);
+
     }
 
 }
