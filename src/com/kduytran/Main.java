9497b1cba0b8516befc112b4907f846ff87af9e1
kduytran443 <trankhanhduy18@gmail.com>
2024-02-26 21:45:55 +0700

    test at testing branch

--- src/com/kduytran/Main.java
+++ src/com/kduytran/Main.java
@@ -6,6 +6,11 @@ public class Main {
         PhoneNumber common = PhoneNumber.of(123, 1234);
         PhoneNumber common2 = PhoneNumber.of(123, 1234);
         System.out.println(common == common2);
+
+        // Test
+        PhoneNumber common3 = PhoneNumber.of(120, 1234);
+        System.out.println(common3);
+        
     }
 
 }
